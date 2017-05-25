8b6f18d6399ec0243b12c4fca692d077256d23f2
Rasmus Enhörning <r.enhorning@gmail.com>
2017-05-25 13:21:07 +0200

    First commit

---
+++ NotifyMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotifyMe
+//
+//  Created by Rasmus Enhörning on 2017-05-25.
+//  Copyright © 2017 Rasmus Enhörning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
